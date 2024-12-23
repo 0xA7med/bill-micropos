503ae6c2d1542a86670cd84b779c6d6ee1ce5766
0xA7med <zeero4123@gmail.com>
2024-12-23 14:21:17 +0200

    Update base URL to match repository name

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/receipt/',
+  base: '/bill-micropos/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
